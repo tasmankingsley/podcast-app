a70486591429ccf05501228f0fcaf445fb88a136
tasmankingsley <tasman.kingsley@gmail.com>
2022-04-06 14:10:53 +1000

    update node-fetch

--- functions/search.js
+++ functions/search.js
@@ -1,13 +1,15 @@
 import fetch from 'node-fetch';
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
     let val = event.queryStringParameters.val;
 
     const response = await fetch(`https://itunes.apple.com/search?term=${val}&entity=podcast`);
     const data = await response.json();
-    return {
+    
+    callback(null, {
       statusCode: 200,
       headers: JSON.stringify({ 'Access-Control-Allow-Origin': '*' }),
       body: JSON.stringify({ data: data }),
-    }
-}
+    });
+};
+
